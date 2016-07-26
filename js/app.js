edde7aeb4b822e75ef98df3a75aa9b35359df038
topplethepat <lenore.alford@gmail.com>
2016-07-26 09:30:57 -0700

    extra styles for pretty map

--- js/app.js
+++ js/app.js
@@ -128,12 +128,129 @@
      
 //function to load map and start app
 
- 	function initMap(){       
+ 	function initMap(){ 
+
+  var styles = [
+     {
+         "featureType": "all",
+         "elementType": "geometry",
+        "stylers": [
+             {
+                 "color": "#63b5e5"
+             }
+         ]
+     },
+     {
+         "featureType": "all",
+         "elementType": "labels.text.fill",
+                "stylers": [
+             {
+                 "gamma": 0.01
+             },
+             {
+                 "lightness": 20
+             }
+         ]
+     },
+     {
+         "featureType": "all",
+         "elementType": "labels.text.stroke",
+         "stylers": [
+             {
+                 "saturation": -31
+             },
+             {
+                 "lightness": -33
+             },
+             {
+                 "weight": 2
+             },
+             {
+                 "gamma": 0.8
+             }
+         ]
+     },
+     {
+         "featureType": "all",
+         "elementType": "labels.icon",
+         "stylers": [
+             {
+                 "visibility": "off"
+             }
+        ]
+    },
+     {
+         "featureType": "landscape",
+         "elementType": "geometry",
+         "stylers": [
+             {
+                 "lightness": 30
+             },
+             {
+                 "saturation": 30
+             }
+         ]
+     },
+     {
+         "featureType": "poi",
+         "elementType": "geometry",
+         "stylers": [
+             {
+                 "saturation": 20
+             }
+         ]
+     },
+     {
+         "featureType": "poi.park",
+         "elementType": "geometry",
+        "stylers": [
+             {
+                 "lightness": 20
+             },
+             {
+                 "saturation": -20
+             }
+         ]
+     },
+     {
+         "featureType": "road",
+         "elementType": "geometry",
+        "stylers": [
+             {
+                 "lightness": 10
+             },
+             {
+                 "saturation": -30
+             }
+         ]
+     },
+     {
+         "featureType": "road",
+         "elementType": "geometry.stroke",
+         "stylers": [
+             {
+                 "saturation": 25
+             },
+             {
+                 "lightness": 25
+             }
+         ]
+     },
+     {
+         "featureType": "water",
+         "elementType": "all",
+         "stylers": [
+             {
+                 "lightness": -20
+             }
+         ]
+     }
+  ]      
         	  
        //constructor creates a new map - only center and zoom required
        	map = new google.maps.Map(document.getElementById('map'), {
        		center: {lat: 37.996481, lng: -122.59430600000002},
-          //styles: styles,
+          styles: styles,
        		zoom: 15,
        		mapTypeControl: false
 
